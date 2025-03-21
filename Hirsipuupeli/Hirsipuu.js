5c1d009bbd4bee0a4eb878185f1a99736f356643
Kim Tiainen <gkiti001@edu.xamk.fi>
2025-03-21 12:37:19 +0200

    hirsipuu toimii

--- Hirsipuupeli/Hirsipuu.js
+++ Hirsipuupeli/Hirsipuu.js
@@ -2,22 +2,45 @@
 var sanat = ["kissa", "koira", "auto", "talo", "koulu", "tietokone", "puhelin", "kukka", "kirja", "lehti"];
 var arvattavaSana = sanat[Math.floor(Math.random() * sanat.length)];
 var eläma = 6;
+var arvattuSana = Array(arvattavaSana.length).fill('_').join('');
 
-document.getElementById('arvattavaSana').textContent = arvattavaSana;
+document.getElementById('arvattavaSana').textContent = arvattuSana;
 document.getElementById('elama').textContent = eläma;
+document.getElementById('arvatutKirjaimet').textContent = '';
 
 function Tarkastaja() {
-    var arvaus = document.getElementById('arvaus').value;
-    if (arvattavaSana.includes(arvaus)) {
+    var arvaus = document.getElementById('arvaus').value.toLowerCase();
+    var uusiArvattuSana = '';
+    var oikein = false;
+
+    for (var i = 0; i < arvattavaSana.length; i++) {
+        if (arvattavaSana[i] === arvaus) {
+            uusiArvattuSana += arvaus;
+            oikein = true;
+        } else {
+            uusiArvattuSana += arvattuSana[i];
+        }
+    }
+
+    arvattuSana = uusiArvattuSana;
+    document.getElementById('arvattavaSana').textContent = arvattuSana;
+
+    if (oikein) {
         console.log("Oikein!");
-        return true;
     } else {
         console.log("Väärin!");
         eläma--;
         console.log("Sinulla on " + eläma + " elämää jäljellä.");
         document.getElementById('elama').textContent = eläma;
-        return false;
+        
+        if (eläma == 0) {
+            console.log("Hävisit pelin!");
+            alert("Pään poisto!");
+        }
     }
+
+    document.getElementById('arvatutKirjaimet').textContent += arvaus + ' ';
+    document.getElementById('arvaus').value = '';
 }
 
 console.log(arvattavaSana);
