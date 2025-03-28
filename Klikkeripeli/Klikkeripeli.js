681e289b57e275f1a05c29b298cf3a18b8e5bc29
Tino Skarp <atisk001@edu.xamk.fi>
2025-03-28 13:24:07 +0200

    Lisätty style klikkeripeliin

--- Klikkeripeli/Klikkeripeli.js
+++ Klikkeripeli/Klikkeripeli.js
@@ -2,6 +2,7 @@
 var pisteet = 0;
 var pisteetPerSec = 0;
 var kehitys1Hinta = 10;
+var infoText;
 
 document.getElementById('Pisteet').textContent = pisteet;
 
@@ -14,8 +15,12 @@ console.log(pisteet)
 function Kehitys1(){
     if(pisteet < kehitys1Hinta){
         console.log("Pisteet eivät riitä")
+        infoText = "Pisteet eivät riitä";
+        document.getElementById('infoText').textContent = infoText
         return;
     }
+infoText = "";
+document.getElementById('infoText').textContent = infoText;
 pisteet = pisteet - kehitys1Hinta
 document.getElementById('Pisteet').textContent = pisteet;
 kehitys1Hinta = kehitys1Hinta * 3;
